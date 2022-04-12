f6b3d4ebb7bd6d970cb05d28163c56db19a905b7
Elisee-des <esabidani@gmail.com>
2022-04-12 12:18:41 +0000

    Affichage dans l'input le search.fin

--- src/App.js
+++ src/App.js
@@ -5,20 +5,23 @@ import Card from './components/Card';
 
 const App = () => {
 
-  const [mealsData, setMealsData] = useState([])
+  const [mealsData, setMealsData] = useState([]);
+  const [inputSearch, setInputSearch] = useState("")
 
   useEffect(() => {
-    axios.get("https://www.themealdb.com/api/json/v1/1/search.php?s=chicken")
+    axios.get("https://www.themealdb.com/api/json/v1/1/search.php?s=" + inputSearch)
       .then((res) => setMealsData(res.data.meals))
-  }, [])
+  }, [inputSearch])
 
   return (
     <dvi className="app-container">
       <h1>React Cooking App</h1>
-      <input type="text" placeholder="Tapez le nom d'un aliment(en anglais)" />
-      <div className="meals container">
-        {
-          mealsData.map((meal) => (<Card key={meal.idMeal} meal={meal}/>))
+      <input type="text" placeholder="Tapez le nom d'un aliment(en anglais)" onChange={(e) => setInputSearch(e.target.value)}/>
+      <div className="meals-container">
+        {mealsData &&
+          mealsData
+            .slice(0, 25)
+            .map((meal) => (<Card key={meal.idMeal} meal={meal}/>))
         }
       </div>
     </dvi>
