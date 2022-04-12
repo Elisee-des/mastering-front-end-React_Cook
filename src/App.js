02e4c9b1129d9d7f4e34b191b2f18d7d8c5225cf
Elisee-des <esabidani@gmail.com>
2022-04-12 11:31:08 +0000

    Affichage de l'api.ecriture du CSS

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import Card from './components/Card';
 
 const App = () => {
+
+  const [mealsData, setMealsData] = useState([])
+
+  useEffect(() => {
+    axios.get("https://www.themealdb.com/api/json/v1/1/search.php?s=chicken")
+      .then((res) => setMealsData(res.data.meals))
+  }, [])
+
   return (
     <dvi className="app-container">
       <h1>React Cooking App</h1>
       <input type="text" placeholder="Tapez le nom d'un aliment(en anglais)" />
       <div className="meals container">
-        
+        {
+          mealsData.map((meal) => (<Card key={meal.idMeal} meal={meal}/>))
+        }
       </div>
     </dvi>
   );
--- src/components/Card.js
+++ src/components/Card.js
@@ -4,8 +4,8 @@ const Card = ({ meal }) => {
   return (
     <div className="meal-card">
       <h2>{meal.strMeal}</h2>
-      <p>Origin : {meal.strArea}</p>
-      <img src={meal.strMealThumb} alt={"photo " + meal.strMeal} />
+      <p>Origin: {meal.strArea}</p>
+      <img src={meal.strMealThumb} alt={meal.strMeal} />
       <p>{meal.strInstructions}</p>
     </div>
   );
